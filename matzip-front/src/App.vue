e9c0ebef459c05f6254f4dd521bda97c77c9676c
wildcat222 <wildcat222@naver.com>
2024-10-21 17:46:20 +0900

    chore: fontawesome 설치 및 import

--- matzip-front/src/App.vue
+++ matzip-front/src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
+import Header from "@/component/common/Header/Header.vue";
 
 </script>
 
 <template>
+  <header>
+    <Header/>
+  </header>
+  <main>
 
+  </main>
+  <footer>
+
+  </footer>
 </template>
 
 <style scoped>
-
+#app {
+  text-align: center;
+}
 </style>
