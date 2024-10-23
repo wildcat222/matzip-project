0aab7d401547b877056d7ae794877599b89f9a90
wildcat222 <wildcat222@naver.com>
2024-10-24 05:17:48 +0900

    feat: pinia 라이브러리 추가 및 사용

--- matzip-front/src/main.js
+++ matzip-front/src/main.js
@@ -6,7 +6,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import '@fortawesome/fontawesome-free/css/all.css';
 
+
+import { createPinia } from 'pinia';
+
 const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+
 app.use(router);
 app.use(BootstrapVue3);
 app.mount('#app');
