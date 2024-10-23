febf48dba30b724f1b4a3f9ab5097ba39b750404
WhooGeek <rlarjswn0303@naver.com>
2024-10-23 11:52:45 +0900

    refactor: #7 main.js 라우터 임포트 추가

--- matzip-front/src/main.js
+++ matzip-front/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/router'
+import router from './router/router.js'
 import { BootstrapVue3 } from "bootstrap-vue-3"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
