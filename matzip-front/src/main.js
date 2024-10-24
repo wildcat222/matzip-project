70c17c020db7eafb52444c4262c9c033a8dc551c
leebackcoding <lcy2825@gmail.com>
2024-10-24 19:24:40 +0900

    feat: pinia 추가

--- matzip-front/src/main.js
+++ matzip-front/src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import { BootstrapVue3 } from "bootstrap-vue-3"
+import { createPinia } from 'pinia';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import '@fortawesome/fontawesome-free/css/all.css';
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
 app.use(BootstrapVue3);
+app.use(pinia);
 app.mount('#app');
 
