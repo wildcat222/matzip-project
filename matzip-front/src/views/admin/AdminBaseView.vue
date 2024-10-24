f46ff4aacd1d702d44ed36d4c952c731ba1ddad0
leebackcoding <lcy2825@gmail.com>
2024-10-24 19:21:49 +0900

    feat: 관리자 헤더, 네이게이션바 구현

--- matzip-front/src/views/admin/AdminBaseView.vue
+++ matzip-front/src/views/admin/AdminBaseView.vue
@@ -1,11 +1,21 @@
 <script setup>
 
+import AdminHeader from "@/components/admin/common/header/AdminHeader.vue";
+import AdminCommonNav from "@/components/admin/common/nav/AdminCommonNav.vue";
 </script>
 
 <template>
-  $END$
+  <header>
+    <AdminHeader/>
+  </header>
+  <div class="row">
+      <AdminCommonNav/>
+      <RouterView/>
+  </div>
+
 </template>
 
 <style scoped>
+
 
 </style>
