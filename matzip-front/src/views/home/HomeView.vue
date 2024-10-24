0ddd51c706c19b151c01e040852b66d70d6d6f1e
whoogeek <rlarjswn0303@naver.com>
2024-10-25 00:06:14 +0900

    feat: #22 메인페이지 HomeView.vue 인기리스트 코드 추가

--- matzip-front/src/views/home/HomeView.vue
+++ matzip-front/src/views/home/HomeView.vue
@@ -1,16 +1,20 @@
 <template class="main-template">
   <div class="main-container">
+
     <h1 class="space-text">Space</h1>
+<!--     맵 & 인기태그-->
     <div class="main-map">
       <current-map />
       <hot-tag :tags="tags" />
     </div>
-    <!--    <div class="main-list">-->
-    <!--      <hot-list :items="hotListItem" />-->
-    <!--    </div>-->
-    <!--    <div class="main-post">-->
-    <!--      <home-post :boards="boards" />-->
-    <!--    </div>-->
+<!--  인기 리스트  -->
+    <div class="main-list">
+      <hot-list :items="hotListItem" />
+    </div>
+<!--  게시판  -->
+    <div class="main-post">
+      <home-post :boards="boards" />
+    </div>
   </div>
 </template>
 
@@ -19,50 +23,56 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import HotTag from '@/components/main/HotTag.vue';
 import CurrentMap from '@/components/main/CurrentMap.vue';
-// import HotList from '@/components/main/HotList.vue';
-// import HomePost from '@/components/main/HomePost.vue';
+import HotList from '@/components/main/HotList.vue';
+import HomePost from '@/components/main/HomePost.vue';
 
 // 인기 태그 배열 선언
 const tags = ref([]);
 const errorMessage = ref(null);
 // 인기 리스트 배열 선언
-// const hotListItem = ref([]);
-// 게시판 배열 선언
-// const boards = ref([]);
+const hotListItem = ref([]);
+게시판 배열 선언
+const boards = ref([]);
 
 
 // 인기 태그 마운트 될때 데이터 가져오기
 onMounted(async () =>{
   try{
     // 인기 태그 가져오기
-    const response = await axios.get('http://localhost:8000/back/api/v1/posts/top-10-tags');
-    if(response.data && response.data.data2 && response.data.data2.tags){
-      tags.value = response.data.data2.tags;
-      console.log(tags.value);
+    const tagResponse = await axios.get('http://localhost:8000/back/api/v1/posts/top-10-tags');
+    if(tagResponse.data && tagResponse.data.data2 && tagResponse.data.data2.tags){
+      tags.value = tagResponse.data.data2.tags;
     }else{
       tags.value = []; // 데이터 없을때 빈 배열 나오게 설정
-      errorMessage.value= '리스트가 없습니다.'
+      errorMessage.value= '태그가 없습니다.'
+    }
+
+    // 인기 리스트 가져오기
+    const listResponse = await axios.get('http://localhost:8000/back/api/v1/listBox/top')
+    if(listResponse.data && listResponse.data.data){
+      hotListItem.value = listResponse.data.data;
+
+    }else{
+      hotListItem.value = [];
+      errorMessage.value='리스트가 없습니다.'
+      console.log(hotListItem.value);
     }
 
 
-    // 인기 리스트 가져오기
-    // const listResponse = await axios.get('http://localhost:8000/back/api/v1/listBox/top')
-    // // hotListItem.value = response.data.slice(0,5);
-
     // 게시판 데이터 가져오기
-    //   const page=1;
-    //   const size=2;
-    //   const boardSeq=1; // 초기값 설정, 나중에 증가 가
-    //
-    //   const postResponse = await axios.get('http://localhost:8000/back/api/v1/boards/${boardSeq}/posts',{
-    //     params: { page, size }
-    //   });
-    //
-    //   boards.value = postResponse.data.map(post => ({
-    //     name: post.boardCategoryName,
-    //     recentPosts: post.postTitle
-    //   }));
-    //   console.log(listResponse)
+      const page=1;
+      const size=2;
+      const boardSeq=1; // 초기값 설정, 나중에 증가 가
+
+      const postResponse = await axios.get('http://localhost:8000/back/api/v1/boards/${boardSeq}/posts',{
+        params: { page, size }
+      });
+
+      boards.value = postResponse.data.map(post => ({
+        name: post.boardCategoryName,
+        recentPosts: post.postTitle
+      }));
+      console.log(listResponse)
   } catch(error){
     console.error('데이터 가져오기 실패:', error);
     errorMessage.value = error.response ? error.response.data.message : error.message; // 에러메세지 저장
