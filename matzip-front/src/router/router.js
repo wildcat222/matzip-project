c5c5cbc012ad7ee3d4271514f283fe601d64185d
leebackcoding <lcy2825@gmail.com>
2024-10-24 19:24:58 +0900

    feat: 관리자 라우터 설정

--- matzip-front/src/router/router.js
+++ matzip-front/src/router/router.js
@@ -1,13 +1,51 @@
-import HomeView from "@/views/home/HomeView.vue"
-
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
+import UserSearch from "@/views/admin/admin-views/UserSearch.vue";
+import ActivityLevelSearch from "@/views/admin/admin-views/ActivityLevelSearch.vue";
+import AdminBaseView from "@/views/admin/AdminBaseView.vue";
+import Login from "@/views/Login.vue";
 
 const route = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/", component: HomeView },
-        { path: "/revuew", component: () => import("@/views/review/Review.vue") },
-        { path: "/review/detail", component: () => import("@/views/review/Detail.vue") },
+        {path: "/", component: Login},
+        {path: "/revuew", component: () => import("@/views/review/Review.vue")},
+        {path: "/review/detail", component: () => import("@/views/review/Detail.vue")},
+        {
+            path: "/admin",
+            component: AdminBaseView,
+            children: [
+                {
+                    path: "users",
+                    component: UserSearch
+                },
+                {
+                    path: "active-level",
+                    component: ActivityLevelSearch
+                },
+            ]
+        }
+
+
+        // {
+        //     path: "/admin/report",
+        //     component:
+        // },
+        // {
+        //     path: "/admin/report-reason",
+        //     component:
+        // },
+        // {
+        //     path: "/admin/penalty-detail",
+        //     component:
+        // },
+        // {
+        //     path: "/admin/penalty",
+        //     component:
+        // },
+        // {
+        //     path: "/admin/restaurant",
+        //     component:
+        // },
     ]
 });
 
