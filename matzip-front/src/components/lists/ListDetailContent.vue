8ef9e20bef3c286acd9fb6683540a808f5c77127
whoogeek <rlarjswn0303@naver.com>
2024-10-25 14:08:34 +0900

    feat: #31 리스트 상세조회 ListDetailContent.vue 리스트 내용 컴포넌트 추가

---
+++ matzip-front/src/components/lists/ListDetailContent.vue
@@ -0,0 +1,45 @@
+<script setup>
+  import { defineProps } from 'vue';
+
+  // props 정의
+  const props = defineProps({
+    nickname: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    }
+  });
+</script>
+
+<template>
+  <div class = "list-detail-content">
+    <div class="header">
+      <img src="../../assets/ListMark.png"/>
+      <h3>{{ nickname }}의 리스트</h3>
+    </div>
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<style scoped>
+.list-detail-content {
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin: 20px 0;
+}
+h2 {
+  margin-bottom: 10px;
+}
+h3 {
+  color: #555;
+}
+</style>
