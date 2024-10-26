7095aa52010f4334cdd6f68c109366c5b7942a67
WhooGeek <rlarjswn0303@naver.com>
2024-10-27 02:37:56 +0900

    refactor: #73 메인페이지 HotList.vue css 재구성

--- matzip-front/src/components/main/HotList.vue
+++ matzip-front/src/components/main/HotList.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="hot-list-container">
-    <h2 class="hot-list-title">인기 리스트</h2>
+    <h2 class="hot-list-title">️ 인기 리스트</h2>
     <div class="hot-list-items">
       <div class="hot-list-item"
            v-for="(item, index) in items"
            :key="index"
+           :style="{ backgroundColor: getRandomPastelColor() }"
            @click="goToListPage(item.listSeq)">
         <div class="item-name">{{ item.listTitle }}</div>
         <p>{{ item.userName }}</p>
@@ -31,24 +32,30 @@ const router = useRouter();
 
 // 리스트 페이지로 이동하기
 const goToListPage = (listSeq) => {
-  router.push(`/list/${listSeq}`); // listSeq 값을 URL에 포함해서 이동
+  router.push(`/user/list/detail/${listSeq}`); // listSeq 값을 URL에 포함해서 이동
+};
+
+const getRandomPastelColor = () => {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = 70 + Math.random() * 20; // 70-90% 정도의 채도
+  const lightness = 85 + Math.random() * 10; // 85-95% 정도의 밝기
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 };
 </script>
 
 <style scoped>
 .hot-list-container {
-  margin-top: 150px;
+  margin-top: 50px;
   padding: 20px;
   border-radius: 8px;
   width: 100%;
 }
 
 .hot-list-title {
-  margin: 20px;
-  font-size: 30px;
+  margin: 20px 0 20px 0;
+  font-size: 48px;
   font-weight: bold;
-  margin-bottom: 40px;
-  text-align: center;
+  text-align: left;
 }
 
 .hot-list-items {
@@ -70,5 +77,15 @@ const goToListPage = (listSeq) => {
 
 .item-name {
   font-weight: bold;
+}
+
+.hot-list-items{
+  display: flex;
+  width: 100%;
+  border: 1px solid #ccc; /* 회색 테두리 추가 */
+  border-radius: 13px; /* 테두리 곡률 설정 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* 그림자 추가 */
+  padding: 16px; /* 내부 여백 추가 */
+  background-color: #fff; /* 배경색 추가 */
 }
 </style>
