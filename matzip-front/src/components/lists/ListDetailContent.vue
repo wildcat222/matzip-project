c2965738c5a54c21ce10d6963e94b6d13c1fbd96
WhooGeek <rlarjswn0303@naver.com>
2024-10-27 15:25:11 +0900

    chore: 리스트 상세 조회 css 2차 수정 ListDetailContent.vue

--- matzip-front/src/components/lists/ListDetailContent.vue
+++ matzip-front/src/components/lists/ListDetailContent.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { defineProps } from 'vue';
+import { useRouter } from 'vue-router'; // Vue Router import
 
 // props 정의
 const props = defineProps({
   nickname: {
     type: String,
+    required: true,
+  },
+  userSeq: { // userSeq prop 추가
+    type: Number,
     required: true,
   },
   title: {
@@ -16,13 +21,20 @@ const props = defineProps({
     required: true,
   }
 });
+
+const router = useRouter(); // Router 인스턴스 가져오기
+
+// 닉네임 클릭 시 이동하는 함수
+const navigateToUserList = () => {
+  router.push(`/user/listbox/${props.userSeq}`); // userSeq에 따라 URL로 이동
+};
 </script>
 
 <template>
   <div class="list-detail-content">
     <div class="header">
       <img src="../../assets/ListMark.png" alt="List Mark" />
-      <h3>{{ nickname }}</h3>
+      <h3 @click="navigateToUserList" class="nickname">{{ nickname }}</h3> <!-- 닉네임 클릭 시 이동 -->
     </div>
     <h2>{{ title }}</h2>
     <p class="content">{{ content }}</p>
@@ -38,8 +50,9 @@ const props = defineProps({
 }
 
 h2 {
-  margin-bottom: 10px;
+  margin-bottom: 20px;
   font-size: 24px; /* 제목 크기 조정 */
+  font-weight: bold;
   word-wrap: break-word; /* 단어가 박스를 넘어가지 않도록 설정 */
   overflow-wrap: break-word; /* 글자 사이의 줄 바꿈을 허용 */
 }
@@ -47,6 +60,7 @@ h2 {
 h3 {
   color: #555;
   margin: 0; /* 기본 여백 제거 */
+  cursor: pointer; /* 커서 포인터로 변경 */
 }
 
 img {
